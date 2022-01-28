eb283c71d2a37610e5d5f6ac1ab98e7651608771
Dan Ignatov <ignatovdan@gmail.com>
2022-01-29 00:27:42 +0300

    Implement 01-basics-02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+class calculator {
+    static sum(left, right) {
+        return left + right;
+    }
+
+    static subtract(left, right) {
+        return left - right;
+    }
+
+    static multiply(left, right) {
+        return left * right;
+    }
+
+    static divide(left, right) {
+        return left / right;
+    }
+}
+
+const MyCalculator = defineComponent({
+    data() {
+        return {
+            left: 2,
+            right: 3,
+            operation: 'divide'
+        };
+    },
+
+    computed: {
+        result() {
+            return calculator[this.operation](this.left, this.right);
+        }
+    }
+});
+
+const app = createApp(MyCalculator);
+app.mount('#app');

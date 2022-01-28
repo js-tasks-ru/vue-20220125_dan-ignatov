9265752a6eadb1bd8447820c3d71778b7e23945e
Dan Ignatov <ignatovdan@gmail.com>
2022-01-29 02:05:59 +0300

    Fetch all meetups

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -14,6 +14,18 @@ function fetchMeetupById(meetupId) {
   });
 }
 
+function fetchMeetups() {
+  return fetch(`${API_URL}/meetups`).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return response.json().then((error) => {
+        throw error;
+      });
+    }
+  });
+}
+
 // Требуется создать Vue приложение
 
 const MeetupList = [1, 2, 3, 4, 5];
@@ -32,7 +44,7 @@ const MeetupSelector = {
       immediate: true,
       async handler(newValue) {
         const meetup = await fetchMeetupById(newValue);
-        this.selectedMeetupTitle = meetup.title;
+        this.selectedMeetupTitle = meetup?.title;
       }
       // async selectedMeetupId(newValue) {
       //   const meetup = await fetchMeetupById(newValue);
@@ -44,3 +56,31 @@ const MeetupSelector = {
 
 const app = createApp(MeetupSelector);
 app.mount('#app');
+
+// ============ v2 ============
+
+const MeetupSelector2 = {
+  data() {
+    return {
+      meetupList2: [],
+      selectedMeetupId: undefined, // explicit declaration is required to create a proxied property, track changes and call 'watch'
+    }
+  },
+
+  async mounted() {
+    this.meetupList2 = await fetchMeetups();
+    this.selectedMeetupId = this.meetupList2.length > 0 ? this.meetupList2[0].id : undefined;
+  },
+
+  watch: {
+    selectedMeetupId: {
+      immediate: true,
+      handler(newValue) {
+        this.selectedMeetupTitle = this.meetupList2.find(item => item.id === newValue)?.title;
+      }
+    }
+  }
+};
+
+const app2 = createApp(MeetupSelector2);
+app2.mount('#app2');

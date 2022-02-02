46372fc9cdedc3f4d42178cb627f79a97d35f7f7
Dan Ignatov <ignatovdan@gmail.com>
2022-02-03 02:15:34 +0300

    Implement 03-MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,10 +1,24 @@
+//import string from 'lodash-es/string';
 import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: {
+      type: String,
+    },
+    image: String,
+  },
+
+  computed: {
+    style() {
+      return this.image && `--bg-url: url('${this.image}')`;
+    },
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="style">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });

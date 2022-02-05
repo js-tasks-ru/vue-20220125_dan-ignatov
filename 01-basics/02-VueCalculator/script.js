95f99ba16fa1d7e885b3be24c74510305e9d3a92
Dan Ignatov <ignatovdan@gmail.com>
2022-02-05 17:47:24 +0300

    Fix lint

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -2,38 +2,38 @@ import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
 class Calculator {
-    static sum(left, right) {
-        return left + right;
-    }
+  static sum(left, right) {
+    return left + right;
+  }
 
-    static subtract(left, right) {
-        return left - right;
-    }
+  static subtract(left, right) {
+    return left - right;
+  }
 
-    static multiply(left, right) {
-        return left * right;
-    }
+  static multiply(left, right) {
+    return left * right;
+  }
 
-    static divide(left, right) {
-        return left / right;
-    }
+  static divide(left, right) {
+    return left / right;
+  }
 }
 
 const MyCalculator = defineComponent({
-    data() {
-        return {
-            left: 2,
-            right: 3,
-            operation: 'divide',
-            Calculator
-        };
-    },
+  data() {
+    return {
+      left: 2,
+      right: 3,
+      operation: 'divide',
+      Calculator,
+    };
+  },
 
-    computed: {
-        result() {
-            return Calculator[this.operation](this.left, this.right);
-        }
-    }
+  computed: {
+    result() {
+      return Calculator[this.operation](this.left, this.right);
+    },
+  },
 });
 
 const app = createApp(MyCalculator);

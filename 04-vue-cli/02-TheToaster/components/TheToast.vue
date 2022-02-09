407fbad0c58c77fa547018f471184f0ed9c77eda
Dan Ignatov <ignatovdan@gmail.com>
2022-02-10 00:57:04 +0300

    Toaster + Toast

---
+++ 04-vue-cli/02-TheToaster/components/TheToast.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="toast" :class="cssClass">
+    <ui-icon class="toast__icon" :icon="iconName" />
+    <span>{{ message }}</span>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon';
+
+const ToastTypes = {
+  success: {
+    iconName: 'check-circle',
+    cssClass: 'toast_success',
+  },
+  error: {
+    iconName: 'alert-circle',
+    cssClass: 'toast_error',
+  },
+};
+
+export default {
+  name: 'TheToast',
+
+  components: { UiIcon },
+
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: (name) => Object.keys(ToastTypes).includes(name),
+    },
+  },
+
+  computed: {
+    cssClass() {
+      return ToastTypes[this.type].cssClass;
+    },
+    iconName() {
+      return ToastTypes[this.type].iconName;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast__icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
+}
+</style>
--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,58 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <template v-for="item in items" :key="item.key">
+      <div class="ToasterItem">
+        <the-toast :type="item.type" :message="item.message"></the-toast>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
-import UiIcon from './UiIcon';
+import TheToast from './TheToast';
+
+const leaveTimeout = 5000;
+
+function createToasterDataItem(type, message) {
+  return {
+    key: new Date().getTime() + message + type, // I don't think we need uuid in learning samples
+    type,
+    message,
+    leaveTimeout,
+  };
+}
 
 export default {
   name: 'TheToaster',
 
-  components: { UiIcon },
+  components: { TheToast },
+
+  data() {
+    return {
+      items: new Array(),
+    };
+  },
+
+  beforeUnmount() {
+    this.items?.foreach((item) => clearTimeout(item.timeoutId));
+  },
+
+  methods: {
+    _addItem(type, message) {
+      const newItem = createToasterDataItem(type, message);
+      this.items.push(newItem);
+      newItem.timeoutId = setTimeout(
+        () => (this.items = this.items.filter((item) => item.key !== newItem.key)),
+        leaveTimeout,
+      );
+    },
+    success(message) {
+      this._addItem('success', message);
+    },
+    error(message) {
+      this._addItem('error', message);
+    },
+  },
 };
 </script>
 
@@ -41,33 +75,7 @@ export default {
   }
 }
 
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
+.ToasterItem + .ToasterItem {
   margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>

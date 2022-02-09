c6c93a3647be12d183f60346f76596206718379f
Dan Ignatov <ignatovdan@gmail.com>
2022-02-10 00:56:48 +0300

    Fix lint

--- 04-vue-cli/02-TheToaster/components/TheToaster_SingleComponent.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster_SingleComponent.vue
@@ -55,7 +55,8 @@ export default {
       this.items.push(newItem);
       newItem.timeoutId = setTimeout(
         () => (this.items = this.items.filter((item) => item.key !== newItem.key)),
-        leaveTimeout);
+        leaveTimeout,
+      );
     },
     success(message) {
       this._addItem('success', message);

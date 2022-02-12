18d06b2c371d1b99c924c2e1dc5ee9c21b462950
Dan Ignatov <ignatovdan@gmail.com>
2022-02-12 15:55:23 +0300

    Implement savedPosition and hash

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -41,4 +41,35 @@ export const router = createRouter({
       ],
     },
   ],
+
+  scrollBehavior: (to, from, savedPosition) => {
+    if (!savedPosition) {
+      return false;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    if (to.name === 'index') {
+      return new Promise((resolve) => {
+        // Для PageMeetups.vue с его отложенным созданием DOM элементов на mount использую setTimeout (только для учебы).
+        // Без Promise savedPosition будет обработан синхронно через **window.scrollTo** и прокрутки не будет
+        // потому что PageMeetups.vue еще не создал свои DOM элементы и даже не получил свои данные:
+        // mounted() {
+        //   fetchMeetups().then((meetups) => {
+        //     this.meetups = meetups;
+        //   });
+        // },
+        // В реальном приложении вместо setTimeout нужно переносить загрузку данных на beforeRouterEnter,
+        // (https://stackoverflow.com/questions/69148784/stop-vue-page-from-loading-till-data-fetch-is-loaded)
+        // или передавать savedPosition в компонент что бы он сам выполнил scroll когда он достроит свой DOM,
+        // или использовать store/keep-alive для загрузки данных и рендера полного DOM до вызова этого метода
+        setTimeout(
+          () => resolve(savedPosition),
+          200 /* time to handle "fetch(`${API_URL}/meetups`).then((res) => res.json());" call*/,
+        );
+      });
+    } else {
+      return savedPosition;
+    }
+  },
 });
